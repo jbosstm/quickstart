d9f5fd230ea9fc87bf2cd27da7e14e25d38aaac2
Tom Jenkinson <tom.jenkinson@redhat.com>
2012-03-31 07:14:37 +0100

    BLACKTIE-385 renamed build file

--- blacktie/test/blacktie-vc9x32.bat
+++ blacktie/test/blacktie-vc9x32.bat
@@ -34,7 +34,7 @@ echo "Started server"
 rem CREATE BLACKTIE DISTRIBUTION
 cd %WORKSPACE%\blacktie\test
 for /f "delims=" %%a in ('hostname') do @set MACHINE_ADDR=%%a
-call ant dist -DBT_HOME=%WORKSPACE%\blacktie\dist\ -DVERSION=5.0.0.M2-SNAPSHOT -DJBOSSAS_IP_ADDR=%JBOSSAS_IP_ADDR% -DMACHINE_ADDR=%MACHINE_ADDR% -Dbpa=vc9x32 -DJBOSS_HOME=%WORKSPACE%\jboss-as-7.1.1.Final
+call ant -f $WORKSPACE/blacktie/test/initializeBlackTie.xml -DBT_HOME=%WORKSPACE%\blacktie\dist\ -DVERSION=5.0.0.M2-SNAPSHOT -DMACHINE_ADDR=%MACHINE_ADDR% -DJBOSSAS_IP_ADDR=%JBOSSAS_IP_ADDR% -DJBOSS_HOME=%WORKSPACE%\jboss-as-7.1.1.Final
 IF %ERRORLEVEL% NEQ 0 echo "Failing build 3" & tasklist & call %WORKSPACE%\jboss-as-7.1.1.Final\bin\jboss-cli.bat --connect command=:shutdown & @ping 127.0.0.1 -n 10 -w 1000 > nul & exit -1
 
 rem RUN THE SAMPLES
