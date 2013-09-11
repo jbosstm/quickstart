1eacc5ff1d5fe841d929685111586c93588cf96e
Amos Feng <zfeng@redhat.com>
2013-09-11 15:26:51 +0800

    Revert "Revert "JBTM-1847 Update to start jboss-as with standalone-blacktie.xml configration""
    This reverts commit 65be7cec486aac882aee6f36490a6d29c417e682.

--- blacktie/test/blacktie-vc9x32.bat
+++ blacktie/test/blacktie-vc9x32.bat
@@ -23,7 +23,7 @@ set JBOSS_HOME=
 
 rem START JBOSS
 cd %WORKSPACE%\jboss-as\bin
-start /B standalone.bat -c standalone-full.xml -Djboss.bind.address=%JBOSSAS_IP_ADDR% -Djboss.bind.address.unsecure=%JBOSSAS_IP_ADDR%
+start /B standalone.bat -c standalone-blacktie.xml -Djboss.bind.address=%JBOSSAS_IP_ADDR% -Djboss.bind.address.unsecure=%JBOSSAS_IP_ADDR%
 echo "Started server"
 @ping 127.0.0.1 -n 20 -w 1000 > nul
 
