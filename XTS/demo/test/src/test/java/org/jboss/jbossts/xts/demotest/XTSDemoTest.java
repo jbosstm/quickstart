c61871cd822113d7af32a28e7d8c78394f3d1d78
Tom Jenkinson <tom.jenkinson@redhat.com>
2014-06-11 14:09:59 +0100

    Updated to 4.17.21.Final

--- XTS/demo/test/src/test/java/org/jboss/jbossts/xts/demotest/XTSDemoTest.java
+++ XTS/demo/test/src/test/java/org/jboss/jbossts/xts/demotest/XTSDemoTest.java
@@ -52,7 +52,7 @@ public class XTSDemoTest {
     private static final Logger log = Logger.getLogger(XTSDemoTest.class);
     private static final int IMPLICIT_WAIT_S = 120;
 
-    private static final String XTS_DEMO_DIR = "../ear/target/xts-demo-ear-4.17.21.Final-SNAPSHOT.ear";
+    private static final String XTS_DEMO_DIR = "../ear/target/xts-demo-ear-4.17.21.Final.ear";
     private static final String XTS_DEMO_ARCHIVE = "xts-demo.ear";
     private static final String DEMO_APP_CONTEXT = "xts-demo";
 
