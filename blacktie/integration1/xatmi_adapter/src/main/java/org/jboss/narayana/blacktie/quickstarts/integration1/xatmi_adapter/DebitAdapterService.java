09121db050853b6781265728fa1cf613d29b166b
Michael Musgrove <mmusgrov@redhat.com>
2015-09-22 11:00:01 +0100

    Updated to 5.2.4.Final-SNAPSHOT

--- blacktie/integration1/xatmi_adapter/src/main/java/org/jboss/narayana/blacktie/quickstarts/integration1/xatmi_adapter/DebitAdapterService.java
+++ blacktie/integration1/xatmi_adapter/src/main/java/org/jboss/narayana/blacktie/quickstarts/integration1/xatmi_adapter/DebitAdapterService.java
@@ -56,7 +56,7 @@ public class DebitAdapterService extends MDBBlacktieService implements javax.jms
         try {
             Context ctx = new InitialContext();
             DebitRemote bean = (DebitRemote) ctx
-                    .lookup("java:global/blacktie-quickstarts-integration1-ejb-ear-5.2.3.Final/blacktie-quickstarts-integration1-ejb-5.2.3.Final/DebitBean!org.jboss.narayana.blacktie.quickstarts.integration1.ejb.DebitRemote");
+                    .lookup("java:global/blacktie-quickstarts-integration1-ejb-ear-5.2.4.Final-SNAPSHOT/blacktie-quickstarts-integration1-ejb-5.2.4.Final-SNAPSHOT/DebitBean!org.jboss.narayana.blacktie.quickstarts.integration1.ejb.DebitRemote");
             log.debug("resolved DebitBean");
             resp = bean.debit(acct_no, amount);
         } catch (NamingException e) {
