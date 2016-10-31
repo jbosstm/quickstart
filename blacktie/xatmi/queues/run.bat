7cfafc662730d1c43af07a8f0085d0f6d713bd18
Amos Feng <zfeng@redhat.com>
2016-10-31 11:52:49 +0800

    JBTM-2779 Update to add the license file

--- blacktie/xatmi/queues/run.bat
+++ blacktie/xatmi/queues/run.bat
@@ -1,3 +1,19 @@
+rem JBoss, Home of Professional Open Source
+rem Copyright 2016, Red Hat, Inc., and others contributors as indicated
+rem by the @authors tag. All rights reserved.
+rem See the copyright.txt in the distribution for a
+rem full listing of individual contributors.
+rem This copyrighted material is made available to anyone wishing to use,
+rem modify, copy, or redistribute it subject to the terms and conditions
+rem of the GNU Lesser General Public License, v. 2.1.
+rem This program is distributed in the hope that it will be useful, but WITHOUT A
+rem WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+rem PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+rem You should have received a copy of the GNU Lesser General Public License,
+rem v.2.1 along with this distribution; if not, write to the Free Software
+rem Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+rem MA  02110-1301, USA.
+
 @echo off
 
 echo "Quickstart: Running externally managed queue quickstart"
