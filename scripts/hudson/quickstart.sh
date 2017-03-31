25cad6f95b355421ff99d2cfcd9b241adb2a190f
Michael Musgrove <mmusgrov@redhat.com>
2017-03-31 15:23:38 +0100

    JBTM-2874

--- scripts/hudson/quickstart.sh
+++ scripts/hudson/quickstart.sh
@@ -1,3 +1,20 @@
+
+# JBoss, Home of Professional Open Source
+# Copyright 2016, Red Hat, Inc., and others contributors as indicated
+# by the @authors tag. All rights reserved.
+# See the copyright.txt in the distribution for a
+# full listing of individual contributors.
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU Lesser General Public License, v. 2.1.
+# This program is distributed in the hope that it will be useful, but WITHOUT A
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public License,
+# v.2.1 along with this distribution; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+
 #!/bin/bash
 
 # TODO I am waiting for https://github.com/jbosstm/narayana/pull/1157 to be merged
@@ -20,11 +37,11 @@ function comment_on_pull
 }
 
 function int_env {
-  export COMMENT_ON_PULL=1;
   export GIT_ACCOUNT=jbosstm
   export GIT_REPO=quickstart
   export MFACTOR=2 # double wait timeout period for crash recovery QA tests
-  [ $NARAYANA_CURRENT_VERSION ] || export NARAYANA_CURRENT_VERSION="5.6.0.Final-SNAPSHOT"
+
+  [ $NARAYANA_CURRENT_VERSION ] || export NARAYANA_CURRENT_VERSION="5.6.0.Final-SNAPSHOT" 
 
   PULL_NUMBER=$(echo $GIT_BRANCH | awk -F 'pull' '{ print $2 }' | awk -F '/' '{ print $2 }')
   PULL_DESCRIPTION=$(curl -ujbosstm-bot:$BOT_PASSWORD -s https://api.github.com/repos/$GIT_ACCOUNT/$GIT_REPO/pulls/$PULL_NUMBER)
@@ -57,14 +74,10 @@ function get_bt_dependencies {
       cd -
       fi
   fi
-  set -e
 }
 
 function build_narayana {
   # INITIALIZE ENV
-  export M2_HOME=/usr/local/apache-maven-3.0.4
-  export ANT_HOME=/home/hudson/apache-ant-1.8.2
-  export PATH=$M2_HOME/bin:$ANT_HOME/bin:$PATH
   export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m"
 
   [ $PROFILE ] || PROFILE=BLACKTIE 
@@ -72,33 +85,45 @@ function build_narayana {
   #rm -rf ~/.m2/repository/
   rm -rf narayana
   git clone https://github.com/${NARAYANA_REPO}/narayana.git
+  if [ $? != 0 ]; then
+    comment_on_pull "Checkout failed: $BUILD_URL";
+    exit -1
+  fi
   cd narayana
-  git checkout $NARAYANA_BRANCH
-  WORKSPACE=$PWD PROFILE=$PROFILE ./scripts/hudson/narayana.sh -DskipTests -Pcommunity
-  cd jboss-as
-  WILDFLY_MASTER_VERSION=`awk "/wildfly-parent/ {getline;print;}" pom.xml | cut -d \< -f 2|cut -d \> -f 2`
-  cd ..
-  cd ..
+  WORKSPACE=$PWD COMMENT_ON_PULL="" PROFILE=$PROFILE ./scripts/hudson/narayana.sh -DskipTests -Pcommunity
+  if [ $? != 0 ]; then
+    comment_on_pull "Narayana build failed: $BUILD_URL";
+    exit -1
+  fi
 }
 
-function build_apache-karaf {
+function configure_wildfly {
+  WILDFLY_MASTER_VERSION=10.1.0.Final
+  
   rm -rf wildfly-$WILDFLY_MASTER_VERSION
-  cp -rp narayana/jboss-as/build/target/wildfly-${WILDFLY_MASTER_VERSION}/ .
+  wget -N http://download.jboss.org/wildfly/$WILDFLY_MASTER_VERSION/wildfly-$WILDFLY_MASTER_VERSION.zip
+  unzip wildfly-$WILDFLY_MASTER_VERSION.zip
   export JBOSS_HOME=$PWD/wildfly-$WILDFLY_MASTER_VERSION
   cp $JBOSS_HOME/docs/examples/configs/standalone-xts.xml $JBOSS_HOME/standalone/configuration/
   cp $JBOSS_HOME/docs/examples/configs/standalone-rts.xml $JBOSS_HOME/standalone/configuration/
+}
 
-  rm -rf apache-karaf
+function build_apache-karaf {
   git clone https://github.com/apache/karaf.git apache-karaf
-  cd apache-karaf
-  mvn -Pfastinstall
-  cd ..
+  if [ $? != 0 ]; then
+    comment_on_pull "Karaf clone failed: $BUILD_URL";
+    exit -1
+  fi
+  ./build.sh -f apache-karaf/pom.xml -Pfastinstall
+  if [ $? != 0 ]; then
+    comment_on_pull "Karaf build failed: $BUILD_URL";
+    exit -1
+  fi
 }
 
 function run_quickstarts {
   echo Running quickstarts
-  set +e
-  BLACKTIE_DIST_HOME=$PWD/narayana/blacktie/blacktie/target/ mvn clean install -DskipX11Tests=true
+  BLACKTIE_DIST_HOME=$PWD/narayana/blacktie/blacktie/target/ ./build.sh clean install -DskipX11Tests=true
 
   if [ $? != 0 ]; then
     comment_on_pull "Pull failed: $BUILD_URL";
@@ -106,13 +131,13 @@ function run_quickstarts {
   else
     comment_on_pull "Pull passed: $BUILD_URL"
   fi
-  set -e
 }
 
 int_env
-#comment_on_pull "Started testing this pull request: $BUILD_URL"
-#rebase_quickstart_repo
-#get_bt_dependencies # TODO uncomment after testing
-#build_narayana
-#build_apache-karaf # TODO uncomment after testing
-run_quickstarts # TODO uncomment after testing
+comment_on_pull "Started testing this pull request: $BUILD_URL"
+rebase_quickstart_repo
+get_bt_dependencies # TODO uncomment after testing
+build_narayana
+configure_wildfly
+#build_apache-karaf # JBTM-2820 disable the karaf build
+run_quickstarts
