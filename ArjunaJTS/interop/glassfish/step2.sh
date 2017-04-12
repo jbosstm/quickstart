mmusgrov/quickstart-1

JBTM-2874 GlassFish interop

ArjunaJTS/interop/step2.sh
@@ -1,0 +1,13 @@
+#!/bin/bash
+set -e
+
+source init.sh
+
+# start jboss and put it into JTS mode
+[ -d "$JBOSS_HOME"  ] || fatal "file not found: $JBOSS_HOME"
+cd $JBOSS_HOME
+./bin/standalone.sh -c standalone-full.xml -Djboss.tx.node.id=1 -Dcom.arjuna.ats.jts.transactionServiceId=0 &
+sleep 3
+./bin/jboss-cli.sh --connect --file=$QS_DIR/configure-jts-transactions.cli
+
+# shut it down using the script step7.sh

ArjunaJTS/interop/step3.sh
@@ -1,0 +1,12 @@
+#!/bin/bash
+set -e
+
+source init.sh
+
+# Start a glassfish server
+cd $QS_DIR
+
+#asadmin start-domain --debug domain1 
+asadmin start-domain domain1
+asadmin set configs.config.server-config.network-config.network-listeners.network-listener.http-listener-1.port=7080
+

jbosstm/quickstart

ArjunaJTS/interop/glassfish/step2.sh
@@ -1,0 +1,12 @@
+#!/bin/bash
+source init.sh
+set -ex
+
+# start jboss and put it into JTS mode
+[ -d "$JBOSS_HOME"  ] || fatal "file not found: $JBOSS_HOME"
+cd $JBOSS_HOME
+./bin/standalone.sh -c standalone-full.xml -Djboss.tx.node.id=1 -Dcom.arjuna.ats.jts.transactionServiceId=0 &
+sleep 3
+./bin/jboss-cli.sh --connect --file=$QS_DIR/configure-jts-transactions.cli
+
+# shut it down using the script step7.sh

ArjunaJTS/interop/glassfish/step3.sh
@@ -1,0 +1,13 @@
+#!/bin/bash
+source init.sh
+set -ex
+
+# Start a glassfish server
+cd $QS_DIR
+
+export PATH=$GLASSFISH/bin:$PATH
+
+#asadmin start-domain --debug domain1 
+asadmin start-domain domain1
+asadmin set configs.config.server-config.network-config.network-listeners.network-listener.http-listener-1.port=7080
+