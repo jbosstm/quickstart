0dda1210bf2d86ea352f2f9c4d3f14fd061a8e8e
Michael Musgrove <mmusgrov@redhat.com>
2017-09-06 17:49:48 +0100

    JBTM-2889 STM + Vert.x

--- STM/vertx/src/main/java/demo/demo3/TripVerticleImpl.java
+++ STM/vertx/src/main/java/demo/demo3/TripVerticleImpl.java
@@ -8,7 +8,7 @@ import demo.domain.TheatreService;
 import io.vertx.core.AbstractVerticle;
 
 /**
- * The base class encapsulates the domain and STM specific logic
+ * The base class encapsulates the filter and STM specific logic
  */
 class TripVerticleImpl extends AbstractVerticle {
 
