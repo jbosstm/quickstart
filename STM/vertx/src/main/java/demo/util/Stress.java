6896cfdacf7135ec7cf3d39e220e92eca1ee2545
Michael Musgrove <mmusgrov@redhat.com>
2017-11-24 12:42:34 +0000

    JBTM-2889 STM with Vert.x example

--- STM/vertx/src/main/java/demo/util/Stress.java
+++
@@ -1,9 +0,0 @@
-package demo.util;
-
-import java.util.concurrent.ExecutionException;
-
-public class Stress {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        StressTest.main(new String[]{"requests=100", "parallelism=10", "url=api/theatre/Odeon"});
-    }
-}
