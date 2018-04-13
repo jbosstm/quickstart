653ddc1ae1680529748d39053fdfda648a4bf336
Tom Jenkinson <tom.jenkinson@redhat.com>
2018-04-13 12:02:06 +0100

    JBTM-3008 Upgrade to Tomcat 9

--- dbcp2-and-tomcat/run.sh
+++ dbcp2-and-tomcat/run.sh
@@ -2,17 +2,24 @@
 set -m
 
 export QUICKSTART_NAME=${PWD##*/}
-TOMCAT_VERSION=7.0.82
-wget -nc https://archive.apache.org/dist/tomcat/tomcat-7/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.zip
+TOMCAT_VERSION=9.0.7
+wget -nc https://archive.apache.org/dist/tomcat/tomcat-9/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.zip
 rm -rf apache-tomcat-$TOMCAT_VERSION
 unzip apache-tomcat-$TOMCAT_VERSION.zip
+cp ~/.m2/repository/org/jboss/spec/javax/transaction/jboss-transaction-api_1.2_spec/1.0.0.Final/jboss-transaction-api_1.2_spec-1.0.0.Final.jar apache-tomcat-$TOMCAT_VERSION/lib/
 export TOMCAT_HOME=$(pwd)/apache-tomcat-$TOMCAT_VERSION/
 chmod +x $TOMCAT_HOME/bin/catalina.sh
+
 mvn package
 rm -rf $TOMCAT_HOME/webapps/${QUICKSTART_NAME}/
 cp target/${QUICKSTART_NAME}.war $TOMCAT_HOME/webapps/
-JPDA_SUSPEND=n $TOMCAT_HOME/bin/catalina.sh jpda run &
-sleep 10
+export JPDA_SUSPEND=n
+$TOMCAT_HOME/bin/catalina.sh jpda run &
+if [ "$JPDA_SUSPEND" != "n" ]; then
+  read -p "Press enter to continue" CONTINUE
+else
+  sleep 10
+fi
 
 for i in {1..10}
 do
@@ -31,7 +38,11 @@ curl -f --data "crash" http://localhost:8080/${QUICKSTART_NAME}/crash
 JPDA_SUSPEND=n $TOMCAT_HOME/bin/catalina.sh jpda run &
 
 # verify the recovery
-sleep 5
+if [ "$JPDA_SUSPEND" != "n" ]; then
+  read -p "Press enter to continue" CONTINUE
+else
+  sleep 5
+fi
 x=`curl -s http://localhost:8080/${QUICKSTART_NAME}/recovery`
 
 $TOMCAT_HOME/bin/catalina.sh stop
--- transactionaldriver/transactionaldriver-jpa-and-tomcat/run.sh
+++ transactionaldriver/transactionaldriver-jpa-and-tomcat/run.sh
@@ -2,10 +2,11 @@
 set -e
 
 export QUICKSTART_NAME=${PWD##*/}
-TOMCAT_VERSION=7.0.82
-wget -nc https://archive.apache.org/dist/tomcat/tomcat-7/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.zip
+TOMCAT_VERSION=9.0.7
+wget -nc https://archive.apache.org/dist/tomcat/tomcat-9/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.zip
 rm -rf apache-tomcat-$TOMCAT_VERSION
 unzip apache-tomcat-$TOMCAT_VERSION.zip
+cp ~/.m2/repository/org/jboss/spec/javax/transaction/jboss-transaction-api_1.2_spec/1.0.0.Final/jboss-transaction-api_1.2_spec-1.0.0.Final.jar apache-tomcat-$TOMCAT_VERSION/lib/
 export TOMCAT_HOME=$(pwd)/apache-tomcat-$TOMCAT_VERSION/
 echo "export JAVA_OPTS=\"-Dcom.arjuna.ats.jta.recovery.XAResourceRecovery1=com.arjuna.ats.internal.jdbc.recovery.BasicXARecovery\;abs://$(pwd)/src/main/resources/h2recoveryproperties.xml\ \;1\"" > $TOMCAT_HOME/bin/setenv.sh
 chmod +x $TOMCAT_HOME/bin/catalina.sh
