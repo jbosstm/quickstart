e5f3946f1d3f2aad965ea887b249ba6983ad38aa
Michael Musgrove <mmusgrov@redhat.com>
2019-01-25 10:24:33 +0000

    JBTM-3087 Use the latest LRA snapshot build

--- rts/lra-examples/api-participant/src/main/java/io/narayana/rts/lra/ParticipantDeserializer.java
+++ rts/lra-examples/api-participant/src/main/java/io/narayana/rts/lra/ParticipantDeserializer.java
@@ -1,15 +1,15 @@
 package io.narayana.rts.lra;
 
 import org.eclipse.microprofile.lra.participant.LRAParticipant;
-import org.eclipse.microprofile.lra.participant.LRAParticipantDeserializer;
+//import org.eclipse.microprofile.lra.participant.LRAParticipantDeserializer;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.URL;
 
-public class ParticipantDeserializer implements LRAParticipantDeserializer {
-    @Override
+public class ParticipantDeserializer {//implements LRAParticipantDeserializer {
+//    @Override
     public LRAParticipant deserialize(URL lraId, byte[] data) {
         try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data))) {
             return (LRAParticipant) ois.readObject();
