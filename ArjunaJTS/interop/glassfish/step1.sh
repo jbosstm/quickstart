a9a65adf8c79dbd39407a9599ecac848c11c9afa
Ondra Chaloupka <ochaloup@redhat.com>
2020-03-05 17:13:37 +0100

    [JBTM-3263] glassfish for interop quickstart is available on github

--- ArjunaJTS/interop/glassfish/step1.sh
+++ ArjunaJTS/interop/glassfish/step1.sh
@@ -7,10 +7,12 @@ set -e
 function build_wf {
   WS=$WORKSPACE
 
-  WORKSPACE="$QS_DIR/wildfly/"
+  cd "$QS_DIR/tmp/"
+  WORKSPACE="$QS_DIR/tmp/wildfly/"
 
+  rm -rf "$WORKSPACE"
   git clone --depth=1 https://github.com/wildfly/wildfly.git
-  cd $WORKSPACE  
+  cd $WORKSPACE
 
   git apply $QS_DIR/interop.wildfly.diff
 
@@ -18,7 +20,7 @@ function build_wf {
   
 
   WILDFLY_MASTER_VERSION=`awk '/wildfly-parent/ { while(!/<version>/) {getline;} print; }' pom.xml | cut -d \< -f 2|cut -d \> -f 2`
-  cp -rp build/target/wildfly-$WILDFLY_MASTER_VERSION/ ${QS_DIR}/
+  cp -rp build/target/wildfly-$WILDFLY_MASTER_VERSION/ ${QS_DIR}/tmp/
   
   cd ${QS_DIR}
   rm -rf $WORKSPACE
@@ -30,14 +32,15 @@ function build_gf {
   GLASSFISH=${GLASSFISH:=xyz}
   if [ ! -d $GLASSFISH ]; then
     echo looking for glassfish
-    # if we are not running using jenkins CI then get the GlassFish sources via svn
+    # if we are not running using jenkins CI then get the GlassFish sources via git clone
     if [ -z ${JENKINS_HOST+x} ]; then
-      echo getting glassfish via svn
-      svn checkout https://svn.java.net/svn/glassfish~svn/trunk/main $QS_DIR/tmp/glassfish4
+      echo "getting glassfish via git clone; QS_DIR is at $QS_DIR"
+      git clone --depth=1 https://github.com/javaee/glassfish -b 4.1.2 $QS_DIR/tmp/glassfish4
       cd $QS_DIR/tmp/glassfish4
       patch -p0 -i $QS_DIR/GLASSFISH-21532.diff  
       mvn install -B -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -DskipTests  
       export GLASSFISH=$QS_DIR/tmp/glassfish4/appserver/distributions/glassfish/target/stage/glassfish4
+      cd -
     fi
   fi
 
