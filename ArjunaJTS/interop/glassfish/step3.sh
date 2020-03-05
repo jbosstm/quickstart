79c587ccad86f59f54d3647c3575440699cc6869
Ondra Chaloupka <ochaloup@redhat.com>
2020-03-05 17:13:37 +0100

    [JBTM-3263] glassfish for interop quickstart is available on github

--- ArjunaJTS/interop/glassfish/step3.sh
+++ ArjunaJTS/interop/glassfish/step3.sh
@@ -2,12 +2,11 @@
 source init.sh
 set -ex
 
-# Start a glassfish server
-cd $QS_DIR
+# start jboss and put it into JTS mode
+[ -d "$JBOSS_HOME"  ] || fatal "file not found: $JBOSS_HOME"
+cd $JBOSS_HOME
+./bin/standalone.sh -c standalone-full.xml -Djboss.tx.node.id=1 -Dcom.arjuna.ats.jts.transactionServiceId=0 &
+sleep 10
+./bin/jboss-cli.sh --connect --file=$QS_DIR/configure-jts-transactions.cli
 
-export PATH=$GLASSFISH/bin:$PATH
-
-#asadmin start-domain --debug domain1 
-asadmin start-domain domain1
-asadmin set configs.config.server-config.network-config.network-listeners.network-listener.http-listener-1.port=7080
-
+# shut it down using the script step7.sh
