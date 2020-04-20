a614d088b1543d37117cfcbae8d4f56b1a5cf57e
Mayank Kunwar <msingh722@gmail.com>
2020-04-20 20:39:51 +0530

    Adding JDK profile and Parent dependencies refactored

--- dbcp2-and-tomcat/run.sh
+++ dbcp2-and-tomcat/run.sh
@@ -1,20 +1,28 @@
 #/bin/bash
 set -m
+set -x
+
+CURL_IP_OPTS=""
+IP_OPTS="${IPV6_OPTS}" # use setup of IPv6 if it's defined, otherwise go with IPv4
+if [ -z "$IP_OPTS" ]; then
+  IP_OPTS="-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses"
+  CURL_IP_OPTS="-4"
+fi
 
 export QUICKSTART_NAME=${PWD##*/}
-TOMCAT_VERSION=9.0.7
-wget -nc https://archive.apache.org/dist/tomcat/tomcat-9/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.zip
+TOMCAT_VERSION=9.0.34
+wget -nc -nv https://archive.apache.org/dist/tomcat/tomcat-9/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.zip
 rm -rf apache-tomcat-$TOMCAT_VERSION
 unzip apache-tomcat-$TOMCAT_VERSION.zip
 cp ~/.m2/repository/org/jboss/spec/javax/transaction/jboss-transaction-api_1.2_spec/1.0.0.Final/jboss-transaction-api_1.2_spec-1.0.0.Final.jar apache-tomcat-$TOMCAT_VERSION/lib/
-export TOMCAT_HOME=$(pwd)/apache-tomcat-$TOMCAT_VERSION/
-chmod +x $TOMCAT_HOME/bin/catalina.sh
+export TOMCAT_HOME="$(pwd)/apache-tomcat-$TOMCAT_VERSION"
+chmod +x "${TOMCAT_HOME}/bin/catalina.sh"
 
 mvn package
-rm -rf $TOMCAT_HOME/webapps/${QUICKSTART_NAME}/
-cp target/${QUICKSTART_NAME}.war $TOMCAT_HOME/webapps/
+rm -rf "${TOMCAT_HOME}/webapps/${QUICKSTART_NAME}/"
+cp target/${QUICKSTART_NAME}.war "${TOMCAT_HOME}/webapps/"
 export JPDA_SUSPEND=n
-$TOMCAT_HOME/bin/catalina.sh jpda run &
+"${TOMCAT_HOME}/bin/catalina.sh" jpda run &
 if [ "$JPDA_SUSPEND" != "n" ]; then
   read -p "Press enter to continue" CONTINUE
 else
@@ -23,19 +31,19 @@ fi
 
 for i in {1..10}
 do
-  curl -f --data "test$i" http://localhost:8080/${QUICKSTART_NAME}
+  curl ${CURL_IP_OPTS} --data "test$i" http://localhost:8080/${QUICKSTART_NAME}
 done
 
-curl -f http://localhost:8080/${QUICKSTART_NAME}
+curl ${CURL_IP_OPTS} http://localhost:8080/${QUICKSTART_NAME}
 
 # remove all strings
-curl -f -X DELETE http://localhost:8080/${QUICKSTART_NAME}
+curl ${CURL_IP_OPTS} -X DELETE http://localhost:8080/${QUICKSTART_NAME}
 
 # crash the application
-curl -f --data "crash" http://localhost:8080/${QUICKSTART_NAME}/crash
+curl ${CURL_IP_OPTS} --data "crash" http://localhost:8080/${QUICKSTART_NAME}/crash
 
 # restart the Tomcat
-JPDA_SUSPEND=n $TOMCAT_HOME/bin/catalina.sh jpda run &
+JPDA_SUSPEND=n "${TOMCAT_HOME}/bin/catalina.sh" jpda run &
 
 # verify the recovery
 if [ "$JPDA_SUSPEND" != "n" ]; then
@@ -43,13 +51,13 @@ if [ "$JPDA_SUSPEND" != "n" ]; then
 else
   sleep 5
 fi
-x=`curl -s http://localhost:8080/${QUICKSTART_NAME}/recovery`
+recovery_response=`curl ${CURL_IP_OPTS} -s http://localhost:8080/${QUICKSTART_NAME}/recovery`
 
-$TOMCAT_HOME/bin/catalina.sh stop
+"${TOMCAT_HOME}/bin/catalina.sh" stop
 rm -rf apache-tomcat-$TOMCAT_VERSION
 
-if [ "$x" != "[\"crash\"]" ]; then
-    echo "Crash and Recovery failed"
+if [ "${recovery_response}" != "[\"crash\"]" ]; then
+    echo "Crash and Recovery failed, response on calling ${QUICKSTART_NAME}/recovery was: ${recovery_response}"
     exit -1
 fi
 
