87c7583915c6312262232f64b918b92803248a49
Ondra Chaloupka <ochaloup@redhat.com>
2020-05-20 07:10:42 +0200

    Updated to 5.10.6.Final-SNAPSHOT

--- scripts/hudson/quickstart.sh
+++ scripts/hudson/quickstart.sh
@@ -24,7 +24,7 @@ function fatal {
 [ $WORKSPACE ] || fatal "please set WORKSPACE to the quickstarts directory"
 NARAYANA_REPO=${NARAYANA_REPO:-jbosstm}
 NARAYANA_BRANCH="${NARAYANA_BRANCH:-master}"
-QUICKSTART_NARAYANA_VERSION=${QUICKSTART_NARAYANA_VERSION:-5.10.5.Final}
+QUICKSTART_NARAYANA_VERSION=${QUICKSTART_NARAYANA_VERSION:-5.10.6.Final-SNAPSHOT}
 
 function comment_on_pull
 {
@@ -46,7 +46,7 @@ function int_env {
   export GIT_REPO=quickstart
   export MFACTOR=2 # double wait timeout period for crash recovery QA tests
 
-  [ $NARAYANA_CURRENT_VERSION ] || export NARAYANA_CURRENT_VERSION="5.10.5.Final" 
+  [ $NARAYANA_CURRENT_VERSION ] || export NARAYANA_CURRENT_VERSION="5.10.6.Final-SNAPSHOT" 
 
   PULL_NUMBER=$(echo $GIT_BRANCH | awk -F 'pull' '{ print $2 }' | awk -F '/' '{ print $2 }')
   PULL_DESCRIPTION=$(curl -ujbosstm-bot:$BOT_PASSWORD -s https://api.github.com/repos/$GIT_ACCOUNT/$GIT_REPO/pulls/$PULL_NUMBER)
@@ -116,7 +116,7 @@ function build_narayana {
     exit -1
   fi
   echo "Deleting check out - assuming all artifacts are in the .m2"
-  cp -rp narayana-full/target/narayana-full-5.10.5.Final-bin.zip $WORKSPACE
+  cp -rp narayana-full/target/narayana-full-5.10.6.Final-SNAPSHOT-bin.zip $WORKSPACE
   cd ..
   rm -rf narayana
 }
