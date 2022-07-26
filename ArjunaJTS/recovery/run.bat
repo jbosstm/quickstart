3782e3f914a0ea85b6785a6e98961e38e2f37c3e
Marco Sappé Griot <msappegr@redhat.com>
2022-07-26 16:37:42 +0200

    [JBTM-2968] Some JTS quickstarts run with JacORB

--- ArjunaJTS/recovery/run.bat
+++ ArjunaJTS/recovery/run.bat
@@ -18,16 +18,16 @@ rem MA  02110-1301, USA.
 
 echo "Running JTS recovery quickstart"
 
-rem SUN_CONIG="-Dcom.sun.CORBA.POA.ORBServerId=1 -Dcom.sun.CORBA.POA.ORBPersistentServerPort=12567"
+OPENJDK_CONFIG="-Dcom.sun.CORBA.POA.ORBServerId=1 -Dcom.sun.CORBA.POA.ORBPersistentServerPort=12567"
 
 IF NOT %QUICKSTART_NARAYANA_VERSION%x == x SET NARAYANA_VERSION_PARAM="-Dversion.narayana=${QUICKSTART_NARAYANA_VERSION}"
 
 echo "Generating a recovery record ..."
-mvn -e compile exec:java -Dexec.mainClass=Test -Dexec.args="-crash" %NARAYANA_VERSION_PARAM%
+mvn -e compile exec:java -Dexec.mainClass=Test -Dexec.args="-crash" %OPENJDK_CONFIG %NARAYANA_VERSION_PARAM%
 IF %ERRORLEVEL% NEQ 0 exit -1
 
 echo "Recovering failed service - this could take up to a minute or so ..."
-mvn -e exec:java -Dexec.mainClass=Test -Dexec.args="-recover -auto" %NARAYANA_VERSION_PARAM%
+mvn -e exec:java -Dexec.mainClass=Test -Dexec.args="-recover -auto" %OPENJDK_CONFIG %NARAYANA_VERSION_PARAM%
 IF %ERRORLEVEL% NEQ 0 exit -1
 
 echo "Testing that the XID data files were removed"
--- ArjunaJTS/standalone/run.bat
+++ ArjunaJTS/standalone/run.bat
@@ -18,11 +18,7 @@ rem MA  02110-1301, USA.
 
 IF NOT %QUICKSTART_NARAYANA_VERSION%x == x SET NARAYANA_VERSION_PARAM="-Dversion.narayana=${QUICKSTART_NARAYANA_VERSION}"
 
-echo "Running JTS standalone using JacORB"
-mvn -e compile exec:java  -Dexec.cleanupDaemonThreads=false -Dexec.mainClass=org.jboss.narayana.jta.quickstarts.TransactionExample %NARAYANA_VERSION_PARAM%
-IF %ERRORLEVEL% NEQ 0 exit -1
-
-echo "Running JTS standalone using JdkORB"
+echo "Running JTS standalone using OpenJDK ORB"
 mvn -e exec:java  -Dexec.cleanupDaemonThreads=false -Dexec.mainClass=org.jboss.narayana.jta.quickstarts.TransactionExample -DOrbPortabilityEnvironmentBean.orbDataClassName=com.arjuna.orbportability.internal.orbspecific.versions.javaidl_1_4 -DOrbPortabilityEnvironmentBean.orbImpleClassName=com.arjuna.orbportability.internal.orbspecific.javaidl.orb.implementations.javaidl_1_4 %NARAYANA_VERSION_PARAM%
 IF %ERRORLEVEL% NEQ 0 exit -1
 
