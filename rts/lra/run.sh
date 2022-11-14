a13ce5de7f806cb84693c4dc5dd3b35c0e292f89
Michael Musgrove <mmusgrov@redhat.com>
2022-11-14 15:37:18 +0000

    Updated to 6.0.0.Final-SNAPSHOT

--- rts/lra/run.sh
+++ rts/lra/run.sh
@@ -60,12 +60,12 @@ function getDebugArgs {
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 case "$(uname)" in
-   CYGWIN*) export NARAYANA_INSTALL_LOCATION=`cygpath -w $(pwd)/narayana-full-5.13.1.Final` ;;
-   *)       export NARAYANA_INSTALL_LOCATION=$(pwd)/narayana-full-5.13.1.Final ;;
+   CYGWIN*) export NARAYANA_INSTALL_LOCATION=`cygpath -w $(pwd)/narayana-full-6.0.0.Final-SNAPSHOT` ;;
+   *)       export NARAYANA_INSTALL_LOCATION=$(pwd)/narayana-full-6.0.0.Final-SNAPSHOT ;;
 esac
 
 rm -rf $NARAYANA_INSTALL_LOCATION
-NARAYANA_ZIP="narayana-full-5.13.1.Final-bin.zip"
+NARAYANA_ZIP="narayana-full-6.0.0.Final-SNAPSHOT-bin.zip"
 [ ! -f "$WORKSPACE/$NARAYANA_ZIP" ] &&\
    echo "There is no Narayana zip at \$WORKSPACE directory at '$WORKSPACE/$NARAYANA_ZIP" && exit 1
 unzip "$WORKSPACE/$NARAYANA_ZIP"
