8f3eaac615374e64bfe7ca171fd25f7133fa4be1
Michael Musgrove <mmusgrov@redhat.com>
2022-12-08 08:32:21 +0000

    JBTM-3724 JDBCStore example

--- ArjunaJTA/object_store/README.md
+++ ArjunaJTA/object_store/README.md
@@ -28,9 +28,10 @@ This is achieved by persisting information in an Object Store. Various implement
 to cater for various application requirements.
 
 1. FileStoreExample shows how to change the store type to a file base store but in directory different from the default;
-2. HornetqStoreExample shows how to use the Hornetq journal for transction logging;
+2. HornetqStoreExample shows how to use the Hornetq journal for transaction logging;
 3. VolatileStoreExample shows how to use an unsafe (because it does not persist logs in the event of
    failures and therefore does not support recovery) in-memory log store implementation.
+4. JDBCStoreExample shows how to use a database for persisting transaction logs. This example uses [H2](https://www.h2database.com/).
 
 ## Usage
 
@@ -44,7 +45,10 @@ or to run individual tests using the maven java exec plugin:
 mvn -e compile exec:java -Dexec.mainClass=org.jboss.narayana.jta.quickstarts.VolatileStoreExample
 mvn -e compile exec:java -Dexec.mainClass=org.jboss.narayana.jta.quickstarts.HornetqStoreExample
 mvn -e compile exec:java -Dexec.mainClass=org.jboss.narayana.jta.quickstarts.FileStoreExample
+mvn -e compile exec:java -Dexec.mainClass=org.jboss.narayana.jta.quickstarts.JDBCStoreExample
 ```
+
+The JDBCStore can be configured programmatically or via a properties file. To use the properties file pass the argument `-DUSE_JBOSSTS_PROPERTIES=true` on the command line.
 
 ## Expected output
 
@@ -61,3 +65,23 @@ If you use the run script then you the line "[INFO] BUILD SUCCESS" should appear
 Each example either changes the object store directory or object store type (or both) and then runs a
 transaction. Each example performs a relevant test to verify that the object store type or directory,
 as appropriate, was used.
+
+## Viewing JDBC store logs
+
+The example uses H2 so you could use the H2 console:
+
+mvn dependency:copy-dependencies # download the H2 jar
+java -cp target/dependency/h2-1.4.195.jar org.h2.tools.Server -tcp -web # start the H2 console
+Now use a browser to navigate to the H2 console: http://192.168.0.14:8082/ and enter the appropriate connection parameters:
+
+```
+Driver Class: `org.h2.Driver`
+JDBC URL: `jdbc:h2:~/<quickstart home>/ArjunaJTA/object_store/target/h2/JBTMDB`
+User Name: `sa`
+Password: `sa`
+```
+
+Once connected you may view pending transactions by running the following SQL query: `SELECT * FROM JBOSSTSTXTABLE`
+The table is likely to be empty unless the quickstart crashes between the prepare and commit calls (which you can arrange by either using a IDE with suitable breakpoints or by changing the `DummyXAResource.java` source file to halt the JVM during the commit call).
+
+Note that H2 restricts the number of connections so if you have the console open you can't run the quickstart and vice versa.
---
+++ ArjunaJTA/object_store/src/main/java/org/jboss/narayana/jta/quickstarts/JDBCStoreExample.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright The Narayana Authors
+ *
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+package org.jboss.narayana.jta.quickstarts;
+
+import com.arjuna.ats.arjuna.common.ObjectStoreEnvironmentBean;
+import com.arjuna.ats.internal.arjuna.objectstore.jdbc.JDBCStore;
+import com.arjuna.ats.internal.arjuna.objectstore.jdbc.accessors.DynamicDataSourceJDBCAccess;
+import com.arjuna.common.internal.util.propertyservice.BeanPopulator;
+import jakarta.transaction.RollbackException;
+import jakarta.transaction.SystemException;
+import jakarta.transaction.TransactionManager;
+import jakarta.transaction.UserTransaction;
+
+public class JDBCStoreExample {
+
+    public static void main(String[] args) throws Exception {
+        // the Narayana transaction store is configurable, set it up to use a database
+        setupStore(Boolean.getBoolean("USE_JBOSSTS_PROPERTIES"));
+
+        UserTransaction utx = com.arjuna.ats.jta.UserTransaction.userTransaction();
+
+        // start a transaction, enlist some resources with it and then commit it
+        utx.begin();
+        enlistResources();
+        utx.commit();
+    }
+
+    public static void enlistResources() throws SystemException, RollbackException {
+        // resource enlistment is performed via the TransactionManager API
+        TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
+        // create two resource, although they won't actually do anything, they will force a log record to be created
+        DummyXAResource xar1 = new DummyXAResource();
+        DummyXAResource xar2 = new DummyXAResource();
+
+        // and enlist them with the transaction
+        tm.getTransaction().enlistResource(xar1);
+        tm.getTransaction().enlistResource(xar2);
+    }
+
+    private static void setupStore(boolean usePropertiesFile) {
+        if (usePropertiesFile) {
+            setupStoreViaPropertiesFile();
+        } else {
+            setupStoreViaConfigBean();
+        }
+    }
+
+    public static void setupStoreViaConfigBean() {
+        final String jdbcStoreClass = JDBCStore.class.getName();
+        final String jdbcAccess = DynamicDataSourceJDBCAccess.class.getName();
+        final String DB_CLASSNAME = "org.h2.jdbcx.JdbcDataSource";
+        final String DB_URL = "jdbc:h2:file:./target/h2/JBTMDB";
+        final String dataSourceJndiName = String.format("%s;ClassName=%s;URL=%s;User=sa;Password=sa",
+                jdbcAccess, DB_CLASSNAME, DB_URL);
+
+        // Narayana has three types of store for persisting different categories of information,
+        // and we will want to use a database for each of them:
+        final ObjectStoreEnvironmentBean defaultActionStoreObjectStoreEnvironmentBean =
+                BeanPopulator.getNamedInstance(ObjectStoreEnvironmentBean.class, null);
+        final ObjectStoreEnvironmentBean stateStoreObjectStoreEnvironmentBean =
+                BeanPopulator.getNamedInstance(ObjectStoreEnvironmentBean.class, "stateStore");
+        final ObjectStoreEnvironmentBean communicationStoreObjectStoreEnvironmentBean =
+                BeanPopulator.getNamedInstance(ObjectStoreEnvironmentBean.class, "communicationStore");
+
+        // set the store type to the JDBC store
+        defaultActionStoreObjectStoreEnvironmentBean.setObjectStoreType(jdbcStoreClass);
+        stateStoreObjectStoreEnvironmentBean.setObjectStoreType(jdbcStoreClass);
+        communicationStoreObjectStoreEnvironmentBean.setObjectStoreType(jdbcStoreClass);
+
+        // set the JNDI name of the datasource to be  used to access the database
+        defaultActionStoreObjectStoreEnvironmentBean.setJdbcAccess(dataSourceJndiName);
+        stateStoreObjectStoreEnvironmentBean.setJdbcAccess(dataSourceJndiName);
+        communicationStoreObjectStoreEnvironmentBean.setJdbcAccess(dataSourceJndiName);
+    }
+
+    public static void setupStoreViaPropertiesFile() {
+        // the same config can be specified using jbossts propertie files:
+        if (System.getProperty("com.arjuna.ats.arjuna.common.propertiesFile") == null) {
+            System.setProperty("com.arjuna.ats.arjuna.common.propertiesFile", "h2-jbossts-properties.xml");
+        }
+    }
+}
