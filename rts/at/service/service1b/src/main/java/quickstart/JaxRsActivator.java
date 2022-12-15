0149aacea0fc63301b33c5de07a1d5e27fe850e4
Michael Musgrove <mmusgrov@redhat.com>
2022-12-15 17:11:52 +0000

    JBTM-3699 remove demo, service1b and service 2b (maybe add them back in again in the next release

--- rts/at/service/service1b/src/main/java/quickstart/JaxRsActivator.java
+++
@@ -1,8 +0,0 @@
-package quickstart;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class JaxRsActivator extends Application {
-}
