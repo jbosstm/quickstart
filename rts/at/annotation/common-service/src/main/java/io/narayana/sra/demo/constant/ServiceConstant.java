1568880560ae36b85afbdb6cd7a25996c1693cbe
moulalis <mshikalw@redhat.com>
2023-05-23 14:23:10 +0530

    [JBTM-3463] split up each service (hotel, flight and trip) so that they each run in their own JVMs,

---
+++ rts/at/annotation/common-service/src/main/java/io/narayana/sra/demo/constant/ServiceConstant.java
@@ -0,0 +1,19 @@
+package io.narayana.sra.demo.constant;
+
+public class ServiceConstant {
+    public static final String HOTEL_PATH = "/hotel";
+    public static final String HOTEL_NAME_PARAM = "hotelName";
+    public static final String HOTEL_BEDS_PARAM = "beds";
+
+    public static final String FLIGHT_PATH = "/flight";
+    public static final String FLIGHT_NUMBER_PARAM = "flightNumber";
+    public static final String ALT_FLIGHT_NUMBER_PARAM = "altFlightNumber";
+    public static final String FLIGHT_SEATS_PARAM = "flightSeats";
+
+    public static final String TRIP_PATH = "/trip";
+    public static final String SERVICE_PORT_PROPERTY = "quarkus.http.port";
+
+    public static final String HOTEL_SERVICE_PORT_PROPERTY = "hotel.quarkus.http.port";
+
+    public static final String FLIGHT_SERVICE_PORT_PROPERTY = "flight.quarkus.http.port";
+}
---
+++ rts/at/annotation/hotel-service/src/main/java/io/narayana/sra/demo/service/HotelService.java
@@ -0,0 +1,21 @@
+package io.narayana.sra.demo.service;
+
+import io.narayana.sra.demo.model.Booking;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import java.util.concurrent.CompletableFuture;
+
+@ApplicationScoped
+public class HotelService extends BookingStore {
+    public Booking book(String bid, String hotel, Integer beds) {
+        Booking booking = new Booking(bid, hotel, beds, "Hotel");
+
+        add(booking);
+
+        return booking;
+    }
+
+    public CompletableFuture<Booking> bookAsync(String bid, String hotel, Integer beds) {
+        return CompletableFuture.supplyAsync(() -> book(bid, hotel, beds));
+    }
+}
