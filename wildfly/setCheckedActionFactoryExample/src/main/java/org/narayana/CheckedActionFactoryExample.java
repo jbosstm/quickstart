c59f9de54087f84a11a861fd69a64f62e3e0520b
marco sappe griot <msappegr@ibm.com>
2026-02-03 10:53:57 +0100

    Fix import

--- wildfly/setCheckedActionFactoryExample/src/main/java/org/narayana/CheckedActionFactoryExample.java
+++ wildfly/setCheckedActionFactoryExample/src/main/java/org/narayana/CheckedActionFactoryExample.java
@@ -3,7 +3,7 @@ package org.narayana;
 import java.io.Serializable;
 
 import jakarta.ejb.EJB;
-import jakarta.faces.bean.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Named;
 
 import org.narayana.handler.MyEJB;
